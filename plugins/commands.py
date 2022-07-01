6a0a85294b2a415005ef9a5e038e4fa15e7cf6ed
Mhbotz <108395184+Mhbotz@users.noreply.github.com>
2022-07-01 22:37:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -227,7 +227,9 @@ async def bot_status(client,message):
 
 @trojanz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    await message.reply_text(
+    await message.reply_sticker(
+            'CAACAgUAAxkBAAEBHLhilcHI9LGFiorY11Cb41HiOT8XxgACbAYAAr4GsFT_LGNUHw4NliQE',
+    await message.text(
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
@@ -255,6 +257,8 @@ async def start(client, message):
 async def help(client, message):
     fmsg = await message.reply_sticker(
             'CAACAgUAAxkBAAEBHLhilcHI9LGFiorY11Cb41HiOT8XxgACbAYAAr4GsFT_LGNUHw4NliQE',
+    fmsg = await message.text(
+        text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=InlinexKeyboardMarkup(
             [
