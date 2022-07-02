45f8267b947fe75fce0582c3ce43fa7535f23fcd
Mhbotz <108395184+Mhbotz@users.noreply.github.com>
2022-07-02 08:58:44 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,51 +7,18 @@ class Script(object):
 
 
     HELP_MSG = """
-<b>Basic Commands;</b>
+<i><b>You Can't Use Me 🙂
 
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
-
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @CV_UPDATEZ</b>
+Powered By @MHlinkz</b></i>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Tessa</b>
+    ABOUT_MSG = """🤖 <b>My Name : Tessa</b>
 
-⭕️<b>Creater :</b> @Unknown 
+👨‍💻 <b>Creater :</b> @MHlinkz
 
-⭕️<b>Language :</b> <code>Python3</code>
+🧮 <b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+📑 <b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
