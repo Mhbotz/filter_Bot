1d93756af7863fcf04d08d9bf4867b74995e42a8
Mhbotz <108395184+Mhbotz@users.noreply.github.com>
2022-07-02 08:54:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -228,8 +228,9 @@ async def bot_status(client,message):
 @trojanz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
    fmsg = await message.reply_photo(
-            'https://telegra.ph/file/a5ad79591812352cde862.jpg')
-   shiva = await message.reply('Fetching stat..'),
+            'https://telegra.ph/file/a5ad79591812352cde862.jpg',
+        caption=Script.START_MSG.format(message.from_user.mention),
+        disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
